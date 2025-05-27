b674fd48977e3f849ed5af66bfaa5e2d243c9c2f
luqisha <ashiqul.islam12@gmail.com>
2025-05-28 05:02:06 +0600

    Return simple 200 OK respose

--- app/main.py
+++ app/main.py
@@ -4,7 +4,26 @@ import socket  # noqa: F401
 def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    print("HTTP Server is running and listening for connection on port 4221")
+
+    try:
+        while True:
+            client_connection, client_address = server_socket.accept()
+            print(f"Connection established with {client_address}")
+
+            request = client_connection.recv(1024).decode('utf-8')
+            if request:
+                print(f"Received request:\n{request}")
+
+            response = "HTTP/1.1 200 OK\r\n\r\n"
+
+            client_connection.sendall(response.encode('utf-8'))
+
+    except KeyboardInterrupt:
+        print("\nServer is shutting down...")
+    finally:
+        server_socket.close()
+        print("Server shut down.")
 
 
 if __name__ == "__main__":
